a0d4a9e2343759b146a56c4c05164028350f6507
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-29 04:25:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,6 @@ def using_delete(instructors, string)
  instructors.delete("Steven")
 end
 
-def using_delete_at(integer)
-  fammous_robots.delete_at(2)
-end 
+def using_delete_at(famous_robots, integer)
+  famous_robots.delete_at(2)
+end
