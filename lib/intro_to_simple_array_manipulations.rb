780c8b5c49205f2bca27dcb03eee14fdbc4d7fe2
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-29 03:37:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@
 while @my_favorite_things.length > 2
   array1 = ["raindrops on roses", "whiskers on kittens"]
   array2 = ["sports cars", "flatiron school"]
-  array1.concat array2
+  array2.concat array1
 end
 return @all_my_favs
  end
