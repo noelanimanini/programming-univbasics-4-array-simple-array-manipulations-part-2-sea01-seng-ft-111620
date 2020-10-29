550030830f173d3f943554c733580a8ba1e2434b
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-29 03:33:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,6 @@
  def using_concat(arrays)
-   [@my_favorite_things, more_favs].concat
-  
+  array1 = ["raindrops on roses", "whiskers on kittens"]
+  array2 = ["sports cars", "flatiron school"]
+  array1.concat array2
+
  end
