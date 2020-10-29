ea96d4cb807aceed5b5826ac3b747dd9bee20d21
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-29 03:41:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(arrays)
  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 
  more_favs = ["sports cars", "flatiron school"]
- 
+
 puts ["raindrops on roses", "whiskers on kittens"].concat([ "sports cars", "flatiron school"])
 end 
