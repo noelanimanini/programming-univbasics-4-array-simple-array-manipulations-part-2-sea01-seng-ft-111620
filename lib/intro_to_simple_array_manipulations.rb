4e4d09445dd308efffc2be4f5ddbd2aaeaea781b
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-29 03:43:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,12 @@
 def using_concat(arrays)
- @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+@my_favorite_things.length = 4 
+@all_my_favs = []
+
+while @my_favorite_things > 2
+@my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 
  more_favs = ["sports cars", "flatiron school"]
 
 puts ["raindrops on roses", "whiskers on kittens"].concat([ "sports cars", "flatiron school"])
+
 end 
